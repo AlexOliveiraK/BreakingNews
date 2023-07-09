0f748c4ea1b02fa37b321486434d04794e2cbf8c
Alex Oliveira <alex.mcaste@gmail.com>
2023-07-09 19:24:41 -0300

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -2,11 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
     console.log("Wait connecting to the database");
-
-    mongoose.connect("mongodb+srv://alexmcaste:alexmcaste@cluster0.9gdw4zx.mongodb.net/?retryWrites=true&w=majority",
-        { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log("MongoDb Atlas Connected"))
-        .catch((error) => console.log(error));
 }
 
 module.exports = connectDatabase;
